42bddd58bdafeb7aefc9c58f2558b8cab0166ef9
Doroszlai Attila <eti@fene.hu>
2015-08-16 11:35:45 +0200

    removing unused imports

--- assignments/assignment2b/src/test/java/org/magnum/mobilecloud/integration/test/VideoSvcApiTest.java
+++ assignments/assignment2b/src/test/java/org/magnum/mobilecloud/integration/test/VideoSvcApiTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
